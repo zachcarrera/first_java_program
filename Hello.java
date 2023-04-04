b4d3294a038dc75130a6d23f722dd475ee15512d
Zach Carrera <zachcarrera@gmail.com>
2023-04-03 20:06:15 -0700

    initial commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("My name is Zach");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Chicago, IL");
+    }
+}
